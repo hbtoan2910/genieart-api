8b05a853a32762efbb06318668404610bb09ceb3
hbtoan2910 <hbtoan2910@gmail.com>
2024-12-03 12:07:20 -0500

    final update

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
-); //server allows access from frontend with port 5173
+); //server allows access from frontend with port 5173 or production domain
 
 app.options("*", cors()); // Enable preflight requests for all routes
 
