942777f048b6373fe8066e9e9814f1a29e70eef3
hbtoan2910 <hbtoan2910@gmail.com>
2024-12-03 09:58:28 -0500

    first commit

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 
 const routes = require("./routes/api");
 
 const port = process.env.PORT || 5500;
 
 // Enable all request
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+); //server allows access from frontend with port 5173
+
+app.options("*", cors()); // Enable preflight requests for all routes
 
 // parsing application/json
-app.use(express.json())
+app.use(express.json());
 // parsing application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/openai", routes)
+app.use("/openai", routes);
 
 app.listen(port, () => {
-  console.log(`Server running at port http://localhost:${port}!`)
+  console.log(`Server running at port http://localhost:${port}!`);
 });
